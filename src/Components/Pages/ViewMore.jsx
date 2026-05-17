15a7711ece261d5eace0c86ed4c1001b77560fbc
worker <worker0999@gmail.com>
2026-05-17 20:45:21 +0530

    viewmore changes

--- src/Components/Pages/ViewMore.jsx
+++ src/Components/Pages/ViewMore.jsx
@@ -1,12 +1,14 @@
 import axios from "axios"
-import { use, useEffect, useState } from "react"
+import {  useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 
 const ViewMore = () => {
 
   let params =  useParams()
 let productId = params.id
+ const navigate = useNavigate();
 
 let [oneProduct, setOneProduct] = useState({})
 
@@ -19,11 +21,28 @@ let fetchProductDetails = async () => {
 useEffect(() => {
     fetchProductDetails()
 }, [])
+
+ let handleClick = () => {
+navigate(`/adminportal/products`);
+ }
+
+console.log(oneProduct)
+
+let {title, price, description, category, image, rating} = oneProduct
   return (
-    <div>
-      
-     <h1>Product ID: {productId}</h1>
+    <div className="viewmore">
+        <button  className="bck-btn" onClick={handleClick}> Back </button>
+    <h1>{title}</h1>
+    <div className="viewmore-container">
+        <div className="viewmore-image"><img src={image} alt="NO Image" /></div>    
+        <div className="viewmore-details">
+            <h2>Price: ${price}</h2>
+            <h3>Category: {category}</h3>
+            <p>{description}</p>
+            <h4>Rating: {rating && rating.rate} / 5</h4>
+        </div>
     </div>
+   </div>
   )
 }
 
